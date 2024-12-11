e4f5180d1a3e2af2bb74acef0ab35303ecc7d51d
austintigerlu <austintigerlu@gmail.com>
2024-12-11 18:26:22 -0500

    fixed configurations

---
+++ README.md
@@ -0,0 +1,8 @@
+After cloning the repo create a venv run these commands in the terminal:
+1. python -m venv myenv
+2. source myenv/bin/activate
+3. pip install -r requirements.txt
+4. touch .env
+
+.env should include secret keys/variables you don't want public
+
--- gamify/settings.py
+++ gamify/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+# Load environment variables from the .env file
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ia$vecvndl60wm32urq$$0l3n9wviazh2!ucnmh7$e52+e^vu7"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+
     "rest_framework",
     "api"
 ]
